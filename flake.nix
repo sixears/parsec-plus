06a11ce373dde47ed30d9946c5c1410e998c497a
Mr. Fluffy <git@sixearsm>
2022-11-19 11:07:13 +0000

    fstat->1.0.2.24

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     base1t.url           = github:sixears/base1t/r0.0.5.33;
     fpath.url            = github:sixears/fpath/r1.3.2.35;
     monaderror-io.url    = github:sixears/monaderror-io/r1.2.5.18;
-    monadio-plus.url     = github:sixears/monadio-plus/r2.5.1.43;
+    monadio-plus.url     = github:sixears/monadio-plus/r2.5.1.44;
     parsec-plus-base.url = github:sixears/parsec-plus-base/r1.0.5.21;
   };
 
